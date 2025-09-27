1f88301a05ee136f2d000dbef7357db5300eea8f
Koyama <koyamamika.me@gmail.com>
2025-09-27 17:27:20 +0900

    Delete app container

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 // App.tsx
 import { CssBaseline, ThemeProvider } from "@mui/material";
-import Container from "@mui/material/Container";
 
 import theme from "./theme";
 
@@ -13,18 +12,13 @@ import Services from "./components/Services";
 export default function App() {
   return (
     <ThemeProvider theme={theme}>
-      <CssBaseline /> reset CSS mặc định trình duyệt
+      <CssBaseline />
       <Header />
 
       {/* Hero fullwidth, không dùng Container */}
       <Hero />
-
-      {/* Các section còn lại vẫn dùng Container để căn lề */}
-      <Container sx={{ mt: 4, mb: 4 }}>
-        <Services />
-        <ContactForm />
-      </Container>
-
+      <Services />
+      <ContactForm />
       <Footer />
     </ThemeProvider>
   );
